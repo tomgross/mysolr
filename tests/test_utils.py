63f8c92336973ac522edbaa692c78757118e5c21
Tom Gross <itconsense@gmail.com>
2013-01-15 17:49:30 +0100

    pep8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding:  utf-8 -*-
 
 import unittest
 from mysolr import mysolr
@@ -8,33 +8,33 @@ from mysolr import mysolr
 class AddXmlTestCase(unittest.TestCase):
 
     def test_add_xml(self):
-        self.assertEqual(mysolr._get_add_xml([{'id':5, 'foo':'bar'}]),
+        self.assertEqual(mysolr._get_add_xml([{'id': 5, 'foo': 'bar'}]),
                          ('<add overwrite="true"><doc><field name="foo">bar'
                           '</field><field name="id">5</field></doc></add>'))
 
     def test_add_xml_nooverwrite(self):
-        self.assertEqual(mysolr._get_add_xml([{'id':5, 'foo':'bar'}],
+        self.assertEqual(mysolr._get_add_xml([{'id': 5, 'foo': 'bar'}],
                          overwrite=False),
                          ('<add overwrite="false"><doc><field name="foo">bar'
                           '</field><field name="id">5</field></doc></add>'))
 
     def test_add_xml_commit_within(self):
-        self.assertEqual(mysolr._get_add_xml([{'id':5, 'foo':'bar'}],
+        self.assertEqual(mysolr._get_add_xml([{'id': 5, 'foo': 'bar'}],
                          commit_within=2.0),
                          ('<add overwrite="true" commitWithin="2.0"><doc>'
                           '<field name="foo">bar</field><field name="id">5'
                           '</field></doc></add>'))
 
     def test_add_xml_commit_doc_boost(self):
-        self.assertEqual(mysolr._get_add_xml([{'id':7, 'foo':'bar'}],
-                         boost_values={'':2.3}),
+        self.assertEqual(mysolr._get_add_xml([{'id': 7, 'foo': 'bar'}],
+                         boost_values={'': 2.3}),
                          ('<add overwrite="true"><doc boost="2.3">'
                           '<field name="foo">bar</field><field name="id">7'
                           '</field></doc></add>'))
 
     def test_add_xml_commit_field_boost(self):
-        self.assertEqual(mysolr._get_add_xml([{'id':7, 'foo':'bar'}],
-                         boost_values={'foo':4.2}),
+        self.assertEqual(mysolr._get_add_xml([{'id': 7, 'foo': 'bar'}],
+                         boost_values={'foo': 4.2}),
                          ('<add overwrite="true"><doc>'
                           '<field name="foo" boost="4.2">bar</field>'
                           '<field name="id">7</field></doc></add>'))
